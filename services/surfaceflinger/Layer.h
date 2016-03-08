4235c0856fe275ac336106660726b17fe7bf943c
Steve Kondik <steve@cyngn.com>
2016-03-07 22:20:27 -0800

    surfaceflinger: Move option to allow GPU-rendered protected buffers
     * This was in ExLayer, which is only available under QTI_BSP.
     * Set persist.gralloc.cp.level3=true to allow the GPU to render
       protected buffers. This may be desirable if a device is using
       RenderEngine instead of HW accel for LiveDisplay.
    
    Change-Id: Iee782ac0e65c308b516b7a48bebe8a440f053aa1

--- services/surfaceflinger/Layer.h
+++ services/surfaceflinger/Layer.h
@@ -313,7 +313,7 @@ public:
                              const State& /*state*/) { }
     virtual void setAcquiredFenceIfBlit(int& /*fenceFd */,
                        HWComposer::HWCLayerInterface& /*layer */) { }
-    virtual bool canAllowGPUForProtected() const { return false; }
+    virtual bool canAllowGPUForProtected() const;
 
     /*
      * returns the rectangle that crops the content of the layer and scales it
@@ -451,6 +451,7 @@ private:
     uint64_t mLastFrameNumberReceived;
     bool mUpdateTexImageFailed; // This is only modified from the main thread
     uint32_t mTransformHint;
+    bool mIsGPUAllowedForProtected;
 };
 
 // ---------------------------------------------------------------------------
